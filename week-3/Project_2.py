51e6a9a3b6d8f48201ab05d5ad34609cf74d5f82
Chisom-Gbulie <chisom.gbulie@pau.edu.ng>
2023-04-11 20:11:37 +0100

    (week-3)

---
+++ week-3/Project_2.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Code to find the root of a Cubic Equation
+A = float(input("Enter the Value of A in the Cubic Equation: "))
+
+B = float(input("Enter the Value of B in the Cubic Equation: "))
+
+C = float(input("Enter the value of C in the Cubic Equation: "))
+
+D = float(input("Enter the Value of D in the Cubic Equation: "))
+
+Dis = (B * 2) * (C * 2) - (4.0 * A)*(C * * 3) - 4.0 * (B * * 3) * D - (27) * (A * * 2) * (D * * 2) + (18 * A * B * C * D)
+Print("The value of the Discriminant is",Dis)
+
+if Dis == 0:
+    print("Real and Same Roots")
+    
+if Dis > 0:
+    print("Real and Different Roots")
+    
+if Dis < 0:
+    print("Complex roots Exist")
+    
+    
+# Code to find the root of a Quadratic Equation
+a = float(input("Enter the Value of A in the Quadratic Equation: "))
+
+b = float(input("Enter the Value of B in the Quadratic Equation: "))
+
+c = float(input("Enter the value of C in the Quadratic Equation: "))
+
+Disc = (b * * 2) - (4 * a * c )
+Print("The value of the Discriminant is",Disc)
+
+
+
+
+
+
+
+    

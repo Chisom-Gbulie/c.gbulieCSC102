a0bd0cda2064106b122d3916019548563a808f62
Chisom-Gbulie <chisom.gbulie@pau.edu.ng>
2023-06-05 16:38:10 +0100

    (week-6 Practices)

---
+++ week-10/Project-1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#An OOP application that allows customers to buy oranges online.
+
+class oranges(self):
+amt1 = ["20"]
+print("The Quantity of oranges available is 20")
+def question(self)
+
+amt0 = str(input("Enter the quantity of oranges you would like to buy"))
+if amt0 > amt1
+
+def oranges(self, amt0)
+amt2 = str(input("Amount entered is more than the quantity of oranges available"))
+
+def question(self)
+
+else
+price = ["100"]
+def oranges()
+total = (amt2 * price)
+return total
+print("Your total sums up to: ", total)
+print("Thank you for your patronage")
+
+
+# In[ ]:
+
+
+
+
